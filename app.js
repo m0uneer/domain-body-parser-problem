2d710ac2a5852427026aeda71a5e5e968ff723df
Mouneer <m.m.mouneer@gmail.com>
2019-02-13 19:51:45 +0200

    Re-interducing the Domain problem

--- app.js
+++ app.js
@@ -41,72 +41,29 @@ const __awaiter = function (thisArg, _arguments, P, generator) {
   });
 };
 
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-// Add this middleware as the first one
 app.use(function(req, res, next) {
   let activeDomain = domain.active ? domain.active : domain.create();
   activeDomain.run(() => {
-    next();
-  });
-});
-
-app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-// And add this middleware here
-app.use((req, res, next) => {
-  if (domain.active.trap) {
-    throw new Error('Context Leaked!');
-  }
-
-  domain.active.trap = {};
-  next();
-});
-
-app.use((req, res, next) => {
-  return __awaiter(this, void 0, void 0, function* () {
-    try {
-      yield new Promise(() => {
-        throw new Error('test');
-      });
+    if (domain.active.trap) {
+      throw new Error('Context Leaked!');
     }
-    catch (err) {
-      next(err);
-    }
+
+    domain.active.trap = {};
+    __awaiter(this, void 0, void 0, function* () {
+      try {
+        yield new Promise(() => {
+          throw new Error('test');
+        });
+      }
+      catch (err) {
+        next(err);
+      }
+    });
   });
-});
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
