4dc2cba1a445e47ebb6d0b6e0377c1eab6124f6b
Mouneer <m.m.mouneer@gmail.com>
2019-02-13 19:28:34 +0200

    Re-interducing the Domain problem

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+
+// tslint:disable-next-line no-require-imports
+const domain = require('domain');
+
+/**
+ * Patching global promise in response to https://github.com/nodejs/node-v0.x-archive/issues/8648
+ */
+class PatchedPromise extends Promise {
+  constructor(executor) {
+    const activeDomain = domain.active;
+    executor = activeDomain && activeDomain.bind(executor) || executor;
+    // call native Promise constructor
+    super(executor);
+    const then = this.then;
+    this.then = function (onFulfilled, onRejected) {
+      if (activeDomain) {
+        onFulfilled = onFulfilled && activeDomain.bind(onFulfilled);
+        onRejected = onRejected && activeDomain.bind(onRejected);
+      }
+      return then.call(this, onFulfilled, onRejected);
+    };
+    const catchy = this['catch'];
+    this['catch'] = function (onRejected) {
+      if (activeDomain) {
+        onRejected = onRejected && activeDomain.bind(onRejected);
+      }
+      return catchy.call(this, onRejected);
+    };
+  }
+}
+
+Promise = PatchedPromise;
+
+const __awaiter = function (thisArg, _arguments, P, generator) {
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+    function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+    function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var domain = require('domain');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,27 +57,37 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 // Add this middleware as the first one
 app.use(function(req, res, next) {
-    let activeDomain = domain.active ? domain.active : domain.create();
-    activeDomain.run(() => {
-        next();
-    });
+  let activeDomain = domain.active ? domain.active : domain.create();
+  activeDomain.run(() => {
+    next();
+  });
 });
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // And add this middleware here
 app.use((req, res, next) => {
-    if (!domain.active.trapArr) {
-        domain.active.trapArr = [];
-    } else {
-        // This is the trap
-        domain.active.trapArr.push({});
-    }
+  if (domain.active.trap) {
+    throw new Error('Context Leaked!');
+  }
 
-    next();
+  domain.active.trap = {};
+  next();
+});
+
+app.use((req, res, next) => {
+  return __awaiter(this, void 0, void 0, function* () {
+    try {
+      yield new Promise(() => {
+        throw new Error('test');
+      });
+    }
+    catch (err) {
+      next(err);
+    }
+  });
 });
 
 app.use('/', indexRouter);
